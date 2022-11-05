77f65a314e48448e0ef1cd342ee21313f1942286
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-11-04 21:47:53 -0400

    remove fs

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
-const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const {readAndAppend, readFromFile, writeToFile} = require('./helpers/fsUtils');
 
